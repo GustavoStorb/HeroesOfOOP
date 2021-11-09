70c440490458b9e4ed038291e12d5ad601432b36
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:17:47 -0300

    Update GameMenu.java

--- com/gustavostorb/heroesofoop/api/GameMenu.java
+++ com/gustavostorb/heroesofoop/api/GameMenu.java
@@ -1,6 +1,6 @@
 package com.gustavostorb.heroesofoop.api;
 
-public abstract class GameMenu<T> {
+public abstract class GameMenu<T> { // clase abstrata para utilizar o execute em outras funções
 
     public abstract T execute();
 
