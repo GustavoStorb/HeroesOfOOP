5e5a44c7f25d55d2f0dac5c9536d6dff1b96be91
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:40:44 -0300

    Update SelectCharacterCreationTypeMenu.java

--- com/gustavostorb/heroesofoop/menu/SelectCharacterCreationTypeMenu.java
+++ com/gustavostorb/heroesofoop/menu/SelectCharacterCreationTypeMenu.java
@@ -6,20 +6,20 @@ import com.gustavostorb.heroesofoop.api.GameMenu;
 import javax.swing.*;
 import java.util.Arrays;
 
-public class SelectCharacterCreationTypeMenu extends GameMenu<GameCharType> {
+public class SelectCharacterCreationTypeMenu extends GameMenu<GameCharType> { // extende a classe GameMenu
 
     // Menu para selecionar o tipo do personagem
     @Override
     public GameCharType execute() {
         String menu = "Escolhe seu tipo:\n1 - Guerreiro\n2 - Mago\n3 - Arqueiro\n0 - Sair";
-        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE);
+        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE); // interface grafica para aparecer as opcoes de tipo  de personagem
         Integer[] validOptions = new Integer[]{
                 0, 1, 2, 3
         };
 
         if(option == null || option.equals("")) return null;
 
-        boolean isValid = Arrays.asList(validOptions).contains(Integer.parseInt(option));
+        boolean isValid = Arrays.asList(validOptions).contains(Integer.parseInt(option)); // verificacao e mensagem de erro caso selecione uma opcao que nao é valida (0,1,2,3 VALIDAS)
         if(!isValid) {
             JOptionPane.showMessageDialog(null, "Opcão Invalida", "AVISO", JOptionPane.ERROR_MESSAGE);
             return this.execute();
@@ -27,7 +27,7 @@ public class SelectCharacterCreationTypeMenu extends GameMenu<GameCharType> {
 
         GameCharType charType = null;
 
-        switch (Integer.parseInt(option)) {
+        switch (Integer.parseInt(option)) { // switch case caso selecione a opcao 1 (Guerreiro) seta o GameCharType.WARRIOR etc.
             case 1: {
                 charType = GameCharType.WARRIOR;
                 break;
