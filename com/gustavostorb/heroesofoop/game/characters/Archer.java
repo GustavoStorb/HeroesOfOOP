b2b3c2b472c0bfb0140365522bf2e89ecb23aa31
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:20:12 -0300

    Update Archer.java

--- com/gustavostorb/heroesofoop/game/characters/Archer.java
+++ com/gustavostorb/heroesofoop/game/characters/Archer.java
@@ -4,7 +4,7 @@ import com.gustavostorb.heroesofoop.api.GameChar;
 import com.gustavostorb.heroesofoop.api.GameCharType;
 import com.gustavostorb.heroesofoop.api.GameWeapon;
 
-public class Archer extends GameChar { // criação da classe do personagem arqueiro junto com atributos de ataque/defesa/vida
+public class Archer extends GameChar { // criação da classe do personagem arqueiro junto com atributos de ataque/defesa/vida, também  extende outra classe GameChar da API
     public Archer(String name, GameWeapon weapon) {
         super(name, GameCharType.ARCHER, weapon, 20.0, 30.0, 300.0);
     }
