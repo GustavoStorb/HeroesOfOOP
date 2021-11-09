0509ee86dcade04158ee4996f9352d6a5697f9ed
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:38:20 -0300

    Update CreateNewGameMenu.java

--- com/gustavostorb/heroesofoop/menu/CreateNewGameMenu.java
+++ com/gustavostorb/heroesofoop/menu/CreateNewGameMenu.java
@@ -5,22 +5,22 @@ import com.gustavostorb.heroesofoop.api.GameMenu;
 import javax.swing.*;
 import java.util.Arrays;
 
-public class CreateNewGameMenu extends GameMenu<Integer> {
+public class CreateNewGameMenu extends GameMenu<Integer> { // extende a classe GameMenu
 
     // Menu para inicial de novo jogo
     @Override
     public Integer execute() {
         String menu = "1 - Nova partida\n0 - Sair do jogo\n";
-        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE);
+        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE); // interface grafica do menu inicial
         Integer[] validOptions = new Integer[]{
                 0, 1
         };
 
-        if(option == null || option.equals("")) return 0;
+        if(option == null || option.equals("")) return 0; // verificação caso opção seja vazia ou null retorna 0 ou seja fecha a interface
 
-        boolean isValid = Arrays.asList(validOptions).contains(Integer.parseInt(option));
+        boolean isValid = Arrays.asList(validOptions).contains(Integer.parseInt(option)); // verificação para verificar se opcao foi valida ou nao
         if(!isValid) {
-            JOptionPane.showMessageDialog(null, "Opcão Invalida", "AVISO", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Opcão Invalida", "AVISO", JOptionPane.ERROR_MESSAGE); // mensagem de erro caso selecione uma opcao que nao é valida (APENAS 0  e 1 SÃO  VALIDAS)
             return this.execute();
         }
 
