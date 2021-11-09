c468baca2b546664ea00278623e464963797ea3d
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:21:22 -0300

    Update Warrior.java

--- com/gustavostorb/heroesofoop/game/characters/Warrior.java
+++ com/gustavostorb/heroesofoop/game/characters/Warrior.java
@@ -6,7 +6,7 @@ import com.gustavostorb.heroesofoop.api.GameWeapon;
 
 public class Warrior extends GameChar {
 
-    public Warrior(String name, GameWeapon weapon) { // criação da classe do personagem guerreiro junto com atributos de ataque/defesa/vida
+    public Warrior(String name, GameWeapon weapon) { // criação da classe do personagem guerreiro junto com atributos de ataque/defesa/vida, também  extende outra classe GameChar da API
         super(name, GameCharType.WARRIOR, weapon, 30.0, 20.0, 180.0);
     }
 
