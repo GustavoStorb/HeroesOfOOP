bc29acfc34de508d229deb1feef4dcbd66302ab0
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 15:10:07 -0300

    Update Mage.java

--- com/gustavostorb/heroesofoop/game/characters/Mage.java
+++ com/gustavostorb/heroesofoop/game/characters/Mage.java
@@ -6,7 +6,7 @@ import com.gustavostorb.heroesofoop.api.GameWeapon;
 
 public class Mage extends GameChar {
 
-    public Mage(String name, GameWeapon weapon) {
+    public Mage(String name, GameWeapon weapon) { // criação da classe do personagem mago junto com atributos de ataque/defesa/vida
         super(name, GameCharType.MAGE, weapon, 20.0, 10.0, 200.0);
     }
 }
