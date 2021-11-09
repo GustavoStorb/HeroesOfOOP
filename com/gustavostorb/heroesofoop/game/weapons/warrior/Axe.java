1ec50190bd43c0ec2f9b8db89df3f0d693634ca4
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 15:03:42 -0300

    Update Axe.java

--- com/gustavostorb/heroesofoop/game/weapons/warrior/Axe.java
+++ com/gustavostorb/heroesofoop/game/weapons/warrior/Axe.java
@@ -4,7 +4,7 @@ import com.gustavostorb.heroesofoop.api.GameWeapon;
 
 public class Axe extends GameWeapon {
 
-    public Axe() {
+    public Axe() { // criação da classe do machado junto com seu nome e atributos
         super("Machado", 17.0, 8.0);
     }
 }
