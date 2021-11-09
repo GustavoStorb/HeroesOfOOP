7283f8d2f3a437f11a753056496fc3ccac518eb5
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-09 20:36:10 -0300

    Update CreateCharacterCreationNameMenu.java

--- com/gustavostorb/heroesofoop/menu/CreateCharacterCreationNameMenu.java
+++ com/gustavostorb/heroesofoop/menu/CreateCharacterCreationNameMenu.java
@@ -5,19 +5,19 @@ import com.gustavostorb.heroesofoop.api.GameMenu;
 import javax.swing.*;
 import java.util.regex.Pattern;
 
-public class CreateCharacterCreationNameMenu extends GameMenu<String> {
+public class CreateCharacterCreationNameMenu extends GameMenu<String> { // extende a classe GameMenu
 
     // Menu para criação do personagem
     @Override
     public String execute() {
         String menu = "Qual será o nome do seu personagem?";
-        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE);
+        String option = JOptionPane.showInputDialog(null, menu, "Heroes Of OOP", JOptionPane.INFORMATION_MESSAGE); // interface grafica para perguntar o nome do personagem
 
-        if(option == null || option.equals("")) return null;
+        if(option == null || option.equals("")) return null; // verifica se voce colocar nada no nome
 
-        boolean isValid = Pattern.matches("^[a-z-A-Z ]*$", option);
+        boolean isValid = Pattern.matches("^[a-z-A-Z ]*$", option); // verifica se voce colocou apenas caracteres no nome
         if(!isValid) {
-            JOptionPane.showMessageDialog(null, "O nome só pode conter letras.", "AVISO", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "O nome só pode conter letras.", "AVISO", JOptionPane.ERROR_MESSAGE); // mensagem de erro caso nao tenha colocado nome ou tenha colocado algum caracter invalido
             return this.execute();
         }
 
