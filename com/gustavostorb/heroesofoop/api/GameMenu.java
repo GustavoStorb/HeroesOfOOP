6117b9548e722bb6435957b047aecfde0288919e
storb <93295816+GustavoStorb@users.noreply.github.com>
2021-11-19 18:23:38 -0300

    Adicionei comentarios

--- com/gustavostorb/heroesofoop/api/GameMenu.java
+++ com/gustavostorb/heroesofoop/api/GameMenu.java
@@ -1,6 +1,6 @@
 package com.gustavostorb.heroesofoop.api;
 
-public abstract class GameMenu<T> { // clase abstrata para utilizar o execute em outras funções
+public abstract class GameMenu<T> { // clase abstrata para utilizar o execute em outras funções T é tipo um Object, tipo o que eu colocar nesse <> ele vai gerar o execute com o retorno q eu defini no T
 
     public abstract T execute();
 
