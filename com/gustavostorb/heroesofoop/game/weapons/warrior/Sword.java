788b408e06af84b4d1a24a6f52c4158d2b495f80
Dodox10 <94086863+Dodox10@users.noreply.github.com>
2021-11-19 16:36:39 -0300

    Add files via upload

---
+++ com/gustavostorb/heroesofoop/game/weapons/warrior/Sword.java
@@ -0,0 +1,10 @@
+package com.gustavostorb.heroesofoop.game.weapons.warrior;
+
+import com.gustavostorb.heroesofoop.api.GameWeapon;
+
+public class Sword extends GameWeapon { // extende a classe GameWeapon
+
+    public Sword() { // criação da classe da espada junto com seu nome e atributos
+        super("Espada", 10.0, 15.0);
+    }
+}
